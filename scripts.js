681a930f436521948e3b548a907763b0e3724633
LariMayer <larissamayer146@gmail.com>
2025-12-30 19:34:01 -0300

    Primeiro commit - adiciona o projeto inicial

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+  playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+  const choices = ['rock', 'paper', 'scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+
+  return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+  console.log('Humano: ' + human + ' | Máquina: ' + machine)
+
+  if (human === machine) {
+    result.innerHTML = "Deu empate!"
+  } 
+  else if (
+  (human === 'rock' && machine === 'scissors') ||
+  (human === 'paper' && machine === 'rock') ||
+  (human === 'scissors' && machine === 'paper')
+) {
+  humanScoreNumber++
+  humanScore.innerHTML = humanScoreNumber
+  result.innerHTML = "Você ganhou! 🎉"
+} 
+else {
+  machineScoreNumber++
+  machineScore.innerHTML = machineScoreNumber
+  result.innerHTML = "Você perdeu para a Alexa! 🤖"
+}
+}
